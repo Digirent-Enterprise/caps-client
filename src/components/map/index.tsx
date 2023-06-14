815664e088bb5eabd39355740bce74c9b0a1b2d3
Vo Thanh Luan <s3822042@rmit.edu.vn>
2023-06-14 22:13:50 +0700

    update

---
+++ src/components/map/index.tsx
@@ -0,0 +1,21 @@
+import { useTranslation } from "next-i18next";
+
+const Component = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="flex flex-col items-center space-y-5 py-4 lg:items-start lg:p-20">
+      <div className=" text-xl font-medium lg:text-3xl">
+        Medical professionals near me
+      </div>
+      <iframe
+        height="450"
+        className="w-full rounded-md border-0"
+        loading="lazy"
+        src="https://www.google.com/maps/embed/v1/search?q=Doctors%20near%20me&key=AIzaSyDzXRnb98ND-kw4Tv7G1-cZliEKgO-JAnw"
+      ></iframe>
+    </div>
+  );
+};
+
+Component.displayName = "Map";
+export default Component;
