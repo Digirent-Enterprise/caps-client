07e2ef18be88d729b21ef4e644ab7e130aeab0d8
Vo Thanh Luan <s3822042@rmit.edu.vn>
2023-07-24 22:12:12 +0700

    update

--- src/shared/footer/index.tsx
+++ src/shared/footer/index.tsx
@@ -12,7 +12,7 @@ const Component = React.memo(() => {
       className="bg-opacity/60 fixed bottom-0 h-14 w-full bg-light-blue p-4 dark:bg-dark-bg-primary md:p-3"
     >
       <div className="mx-auto w-full max-w-screen-xl">
-        <span className="block text-sm text-light-blue-hover dark:text-dark-white sm:text-center">
+        <span className="block text-sm text-white dark:text-dark-white sm:text-center">
           © {new Date().getFullYear()}{" "}
           <Link href="/" className="hover:underline">
             DICA™
