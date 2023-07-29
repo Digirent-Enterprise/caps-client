e89ad77739b9cc4274dc4a844e90dade446849d4
Vo Thanh Luan <s3822042@rmit.edu.vn>
2023-07-29 13:55:14 +0700

    update (#243)

--- src/shared/custom-responsive-card/type.ts
+++ src/shared/custom-responsive-card/type.ts
@@ -1,3 +1,7 @@
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+
 import { IBaseProps } from "@/types";
 
-export interface ICustomResponsiveCardProps extends IBaseProps {}
+export interface ICustomResponsiveCardProps extends IBaseProps {
+  children?: ReactJSXElement;
+}
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,10 +1,7 @@
 import { CSSProperties } from "react";
 
-import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
-
 export interface IBaseProps {
   className?: string;
   style?: CSSProperties;
   onClick?: (x: any) => void;
-  children?: ReactJSXElement;
 }
