860adb6ca692ef7714af5a531bea8da1af6afcbb
Vo Thanh Luan <s3822042@rmit.edu.vn>
2023-08-31 17:00:02 +0700

    Vtlua/vectorstore (#304)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- src/shared/header/index.tsx
+++ src/shared/header/index.tsx
@@ -64,7 +64,7 @@ const Header = () => {
         >
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0">
             <li>
-              <Link href={"/landing-page"}>
+              <Link href={"/"}>
                 <div className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
                   {t("home")}
                 </div>
