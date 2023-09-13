d8bf459a305e070c83c6c7b72cf1d73b21625fa1
s3822042 <s3822042@rmit.edu.vn>
2023-09-14 01:46:28 +0700

    update

--- src/shared/header/index.tsx
+++ src/shared/header/index.tsx
@@ -89,8 +89,8 @@ const Header = () => {
             ) : (
               <li>
                 <Link href={"/chat"}>
-                  <div className="block rounded bg-gray-50 py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
-                    {t("my_health")}
+                  <div className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">
+                    {t("chat")}
                   </div>
                 </Link>
               </li>
