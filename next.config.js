1c60d9ba07a2e7502ea7c95724b99d6e471da543
s3822042 <s3822042@rmit.edu.vn>
2023-09-20 13:27:11 +0700

    update

--- next.config.js
+++ next.config.js
@@ -24,6 +24,7 @@ const nextConfig = {
     BACKEND_SERVER_URL: process.env.BACKEND_SERVER_URL,
     SOCKET_SERVER_URL: process.env.SOCKET_SERVER_URL,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    SSL_PASSPHRASE: process.env.SSL_PASSPHRASE,
   },
 };
 
